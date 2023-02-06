e7e88b155c4901d6eaa35db9da145ac31d37d354
nmarforio <n.marforio@gmail.com>
2023-02-06 11:07:45 +0100

    add the datatype

--- card.js
+++ card.js
@@ -5,20 +5,20 @@ export function createCharacterCard(datatvshow) {
   <div class="card__image-container">
     <img
       class="card__image"
-      src= ${datatvshow.src}
+      src= ${datatvshow.image}
       alt="Rick Sanchez"
     />
     <div class="card__image-gradient"></div>
   </div>
   <div class="card__content">
-    <h2 class="card__title">${"Prova"}</h2>
+    <h2 class="card__title">${datatvshow.name}</h2>
     <dl class="card__info">
       <dt class="card__info-title">Status</dt>
-      <dd class="card__info-description">${"Prova"}</dd>
+      <dd class="card__info-description">${datatvshow.status}</dd>
       <dt class="card__info-title">Type</dt>
-      <dd class="card__info-description">${"Prova"}</dd>
+      <dd class="card__info-description">${datatvshow.type}</dd>
       <dt class="card__info-title">Occurrences</dt>
-      <dd class="card__info-description">${"prova"}</dd>
+      <dd class="card__info-description">${datatvshow.episode.length}</dd>
     </dl>
   </div>`;
   return newCard;
